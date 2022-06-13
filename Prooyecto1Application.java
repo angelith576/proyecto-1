ee746ae0060bfb9782dcb89fc578b387ca48cf75
angelith576 <107219999+angelith576@users.noreply.github.com>
2022-06-13 14:12:08 -0500

    Add files via upload

---
+++ Prooyecto1Application.java
@@ -0,0 +1,13 @@
+package com.example.proyecto1.prooyecto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prooyecto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prooyecto1Application.class, args);
+	}
+
+}
